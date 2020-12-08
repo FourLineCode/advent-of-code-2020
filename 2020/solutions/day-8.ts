bff85933d797a15234d0a8f756dd611c2b72dad7
ItzAkmal <akmal3535.ah@gmail.com>
2020-12-08 13:24:54 +0600

    day-8

---
+++ 2020/solutions/day-8.ts
@@ -0,0 +1,73 @@
+function formatInput(rawInput: string): string[] {
+	return rawInput.split('\r\n')
+}
+
+function getAcc(instructions: string[][]): number {
+	let acc = 0
+	const map: Record<number, boolean> = {}
+	for (let i = 0; i < instructions.length; ) {
+		if (map[i]) break
+		map[i] = true
+		switch (instructions[i][0]) {
+			case 'nop': {
+				i++
+				break
+			}
+			case 'acc': {
+				acc += parseInt(instructions[i][1])
+				i++
+				break
+			}
+			case 'jmp': {
+				i += parseInt(instructions[i][1])
+				break
+			}
+		}
+	}
+	return acc
+}
+
+export function solution1(rawInput: string): number {
+	const input = formatInput(rawInput)
+	const instructions = input.map((ins) => ins.split(' '))
+
+	return getAcc(instructions)
+}
+
+function checkValid(instructions: string[][]): boolean {
+	const map: Record<number, boolean> = {}
+	for (let i = 0; i < instructions.length; ) {
+		if (map[i]) return false
+		map[i] = true
+		switch (instructions[i][0]) {
+			case 'nop': {
+				i++
+				break
+			}
+			case 'acc': {
+				i++
+				break
+			}
+			case 'jmp': {
+				i += parseInt(instructions[i][1])
+				break
+			}
+		}
+	}
+	return true
+}
+
+export function solution2(rawInput: string): number {
+	const input = formatInput(rawInput)
+	const commands = input.map((ins) => ins.split(' '))
+
+	for (let i = 0; i < commands.length; i++) {
+		commands[i][0] === 'nop' ? (commands[i][0] = 'jmp') : commands[i][0] === 'jmp' ? (commands[i][0] = 'nop') : null
+
+		if (checkValid(commands)) return getAcc(commands)
+
+		commands[i][0] === 'nop' ? (commands[i][0] = 'jmp') : commands[i][0] === 'jmp' ? (commands[i][0] = 'nop') : null
+	}
+
+	return 0
+}
